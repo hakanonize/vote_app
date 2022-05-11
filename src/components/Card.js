f4e97a214eeddd368f4ef2cae1afa3fdc8ff685c
Hakan <hakanonize35@gmail.com>
2022-05-11 20:27:07 +0300

    Added Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
-const Card = ({ title, subTitle, points }) => {
+const Card = ({
+  title = 'Stack Overflow',
+  subTitle = 'http://stackoverflow.com',
+  points = 0,
+}) => {
   return (
     <div className='card-holder w-100 p-2 row'>
-      <div className='square col-sm-3'></div>
-      <div className='content col-sm-9'>AA</div>
+      <div className='square border col-sm-3 d-flex justify-content-center align-items-center'>
+        {/* <img src='/assets/plus.svg' style={{ width: '40px' }} alt='' /> */}
+        <div className='d-flex flex-column'>
+          <p style={{ margin: '0', fontSize: '22px' }} className='text-center'>
+            {points}
+          </p>
+          <p style={{ margin: '0' }}>POINTS</p>
+        </div>
+      </div>
+      <div className='content col-sm-9'>
+        <div className='d-flex flex-column h-100 justify-content-between'>
+          <div>
+            <div className='h4' style={{ margin: '0' }}>
+              {title}
+            </div>
+            <div className='text-secondary'>{subTitle}</div>
+          </div>
+
+          <div className='d-flex justify-content-between'>
+            <div className='d-flex'>
+              <img
+                src='/assets/arrow-up-short.svg'
+                style={{ width: '20px' }}
+                alt=''
+              />
+              <div className='text-secondary pt-1'>Up Vote</div>
+            </div>
+            <div className='d-flex'>
+              <img
+                src='/assets/arrow-down-short.svg'
+                style={{ width: '20px' }}
+                alt=''
+              />
+              <div className='text-secondary pt-1'>Down Vote</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
